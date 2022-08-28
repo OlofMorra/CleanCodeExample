7dbf32e50202eafe721ed944824cbfc1a72175fb
Olof Morra <o.morra@codenext21.com>
2022-08-28 14:03:36 +0200

    Voeg test setup toe

---
+++ tests/GildedRoseTest.py
@@ -0,0 +1,16 @@
+import unittest
+
+from GildedRose import GildedRose
+from Item import Item
+
+
+class GildedRoseTest(unittest.TestCase):
+    def test_foo(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("fixme", items[0].n)
+
+
+if __name__ == '__main__':
+    unittest.main()
